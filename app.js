64780856c6c6ca136a1ded781b45b792d2106f7f
Andrew Sardone <andrew.sardone@gmail.com>
2011-09-13 16:24:44 -0400

    Declare 'recent-chirps' view, query it when app loads

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var ddoc = {
   _id: '_design/app'
 };
 
+ddoc.views = {
+  "recent-chirps": {
+    map: function(doc) {
+      if (doc.type === "chirp" && doc.created_at) {
+        emit(doc.created_at, doc);
+      }
+    }
+  }
+};
+
 couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
 
 module.exports = ddoc;
--- attachments/site.js
+++ attachments/site.js
@@ -36,4 +36,29 @@ $.couch.app(function(app) {
     });
   });
 
+  /**
+   * Query the 'recent-chirps' view when this script loads, update the #chirps
+   * element with the results
+   *
+   * <pre>
+   * <code>
+   * > curl -X GET http://couch/db/_design/design-doc/_view/recent-chirps
+   * {"total_rows":4,"offset":0,"rows":[
+   *   {"id":"1315944878223","key":1315944878223,"value":{...}},
+   *   {"id":"1315944966718","key":1315944966718,"value":{...}},
+   *   {"id":"1315945053099","key":1315945053099,"value":{...}},
+   *   {"id":"1315945940161","key":1315945940161,"value":{...}}
+   * ]}
+   * </code>
+   * </pre>
+   */
+  app.view('recent-chirps', {
+    descending: true,
+    success: function(data) {
+      $.log("Recent chirps: ");
+      $.log(data);
+    }
+  });
+
+
 });
